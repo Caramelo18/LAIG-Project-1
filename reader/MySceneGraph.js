9ac399321dbe7c7a116c72ee2d2105764bb4a38f
Caramelo18 <fabiocaramelo18@gmail.com>
2016-09-26 17:56:38 +0100

    Reading information from scene, illumination and textures

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -34,6 +34,28 @@ MySceneGraph.prototype.onXMLReady=function()
 		return;
 	}	
 
+	var sceneError = this.parseScene(rootElement);
+	
+	if (sceneError != null) {
+		this.onXMLError(sceneError);
+		return;
+	}
+	
+	var illuminationError = this.parseIllumination(rootElement);
+	
+	if (illuminationError != null) {
+		this.onXMLError(illuminationError);
+		return;
+	}
+	
+	var texturesError = this.parseTextures(rootElement);
+	
+	if (texturesError != null) {
+		this.onXMLError(texturesError);
+		return;
+	}
+	
+	
 	this.loadedOk=true;
 	
 	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
@@ -65,7 +87,7 @@ MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
 
 	console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
 
-	var tempList=rootElement.getElementsByTagName('list');
+	/*var tempList=rootElement.getElementsByTagName('list');
 
 	if (tempList == null  || tempList.length==0) {
 		return "list element is missing.";
@@ -81,9 +103,105 @@ MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
 		// process each element and store its information
 		this.list[e.id]=e.attributes.getNamedItem("coords").value;
 		console.log("Read list item id "+ e.id+" with value "+this.list[e.id]);
-	};
+	};*/
 
 };
+
+MySceneGraph.prototype.parseScene = function(rootElement)
+{
+	var elems =  rootElement.getElementsByTagName('scene');
+	if (elems == null) {
+		return "scene element is missing.";
+	}
+
+	if (elems.length != 1) {
+		return "either zero or more than one 'scene' element found.";
+	}
+	
+	var scene = elems[0];
+	
+	this.root = this.reader.getString(scene, 'root');
+	this.axis_length = this.reader.getFloat(scene, 'axis_length');
+	
+	console.log("Scene read from file: root = " + this.root + ", axis_length = " + this.axis_length);
+}
+	
+MySceneGraph.prototype.parseIllumination = function(rootElement)
+{
+	var elems =  rootElement.getElementsByTagName('illumination');
+	if (elems == null) {
+		return "illumination element is missing.";
+	}
+	if (elems.length != 1) {
+		return "either zero or more than one 'illumination' element found.";
+	}
+	
+	var ambient = rootElement.getElementsByTagName('ambient');
+	if (ambient == null) {
+		return "ambient element is missing.";
+	}
+	if (ambient.length != 1) {
+		return "either zero or more than one 'ambient' element found.";
+	}
+	
+	var background = rootElement.getElementsByTagName('background');
+	if (background == null) {
+		return "background element is missing.";
+	}
+
+	if (background.length != 1) {
+		return "either zero or more than one 'background' element found.";
+	}
+	
+	elems = elems[0];
+	
+	this.doubleSided = this.reader.getBoolean(elems, 'doublesided');
+	this.local = this.reader.getBoolean(elems, 'local');
+	
+	console.log('Illumination read from file: doubleSided = ' + this.doubleSided + ", local = " + this.local);
+	
+	ambient = ambient[0];
+	this.ambientR = this.reader.getFloat(ambient, 'r');
+	this.ambientG = this.reader.getFloat(ambient, 'g');
+	this.ambientB = this.reader.getFloat(ambient, 'b');
+	this.ambientA = this.reader.getFloat(ambient, 'a');
+	
+	console.log('Illumination read from file: Ambient R = ' + this.ambientR + ", Ambient G = " + this.ambientG + ", Ambient B = " + this.ambientB + ", Ambient A = ", this.ambientA);
+
+	background = background[0];
+	this.backgroundR = this.reader.getFloat(background, 'r');
+	this.backgroundG = this.reader.getFloat(background, 'g');
+	this.backgroundB = this.reader.getFloat(background, 'b');
+	this.backgroundA = this.reader.getFloat(background, 'a');
+	
+	console.log('Illumination read from file: Background R = ' + this.backgroundR + ", Background G = " + this.backgroundG + ", Background B = " + this.backgroundB + ", Background A = ", this.backgroundA);	
+}	
+
+MySceneGraph.prototype.parseTextures = function(rootElement)
+{
+	var textures = rootElement.getElementsByTagName('textures');
+
+	if (textures == null  || textures.length==0) {
+		return "textures element is missing.";
+	}
+	
+	this.textureList=[];
+	
+	var numText = textures[0].children.length;
+	
+	for (var i = 0; i < numText; i++)
+	{
+		var e = textures[0].children[i];
+		// process each element and store its information
+		this.textureList[e.id] = e.attributes.getNamedItem("id").value;
+		this.textureList[e.file] = e.attributes.getNamedItem("file").value;
+		this.textureList[e.s] = e.attributes.getNamedItem("length_s").value;
+		this.textureList[e.t] = e.attributes.getNamedItem("length_t").value;
+		
+		console.log("Texture read from file: ID = " + this.textureList[e.id] + ", File = " + this.textureList[e.file] + ",S Length = " + this.textureList[e.s] + ",T Length = " + this.textureList[e.t]);
+	};
+	
+}
 	
 /*
  * Callback to be executed on any read error
