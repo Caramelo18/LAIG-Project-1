042d8490276fc3bb3fa8bcd3fec1fa7ba74d162b
mferreira96 <pount@sapo.pt>
2016-10-09 17:40:29 +0100

    Added Circle.js

--- reader/main.js
+++ reader/main.js
@@ -11,7 +11,7 @@ function getUrlVars() {
     return vars;
 }	 
 
-serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'Rectangle.js',  'Triangle.js', 'Cylinder.js',
+serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'Rectangle.js',  'Triangle.js', 'Cylinder.js','Circle.js',
 
 main=function()
 {
