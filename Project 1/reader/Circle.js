ed668ac45b0d4b7c5aaf7521e574bc1ca4b3d22b
mferreira96 <pount@sapo.pt>
2016-10-31 00:35:22 +0000

    changes on structure

---
+++ Project 1/reader/Circle.js
@@ -0,0 +1,55 @@
+/**
+ * Cirle
+ * @constructor
+ */
+ function Circle(scene,radius, slices) {
+ 	CGFobject.call(this,scene);
+
+	this.slices = slices;
+	this.radius = radius;
+
+ 	this.initBuffers();
+ };
+
+ Circle.prototype = Object.create(CGFobject.prototype);
+ Circle.prototype.constructor = Circle;
+
+ Circle.prototype.initBuffers = function() {
+
+ 	this.vertices = [];
+ 	this.indices = [];
+ 	this.normals = [];
+	this.texCoords = [];
+
+    var ang=Math.PI*2/this.slices;
+	var angAux=0;
+    var x,y;
+
+	this.vertices.push(0,0,0);
+	this.normals.push(0,0,1);
+	this.texCoords.push(0.5,0.5);
+
+	for(j = 0; j <= this.slices; j++){
+
+        if(j == this.slices){
+	       this.indices.push(0, j, 1);
+	   }else{
+	       x=Math.cos(angAux) * this.radius;
+	       y=Math.sin(angAux) * this.radius;
+
+	       this.vertices.push(x,y,0);
+	       this.normals.push(0,0,1);
+
+		   this.texCoords.push((x/2)+0.5,-(y/2)+0.5);
+		
+	       if(j>0){
+            this.indices.push(0, j, j+1);
+	       }
+	   }
+
+       angAux+=ang;
+	}
+
+ 	this.primitiveType = this.scene.gl.TRIANGLES;
+ 	this.initGLBuffers();
+ };
