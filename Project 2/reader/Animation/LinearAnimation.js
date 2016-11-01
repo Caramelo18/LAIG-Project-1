42977aa3fba214f679e711815e0db47499a49d67
mferreira96 <pount@sapo.pt>
2016-11-01 01:21:01 +0000

    added folder animation and file LinearAnimation

---
+++ Project 2/reader/Animation/LinearAnimation.js
@@ -0,0 +1,9 @@
+function LinearAnimation(id, controlPoints, time){
+  this.id = id;
+  this.controlPoints = controlPoints;
+  this.time = time;
+}
+
+LinearAnimation.prototype.calculateDistance = function () {
+
+};
