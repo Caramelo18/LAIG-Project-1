58a205b7de4a91ee05e9447521fcdc567c728052
mferreira96 <pount@sapo.pt>
2016-11-01 21:07:05 +0000

    working on transformations

--- Project 2/reader/Animation/CircularAnimation.js
+++ Project 2/reader/Animation/CircularAnimation.js
@@ -11,3 +11,14 @@ function CircularAnimation(id,time, center, radius, startang, rotang){
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
 CircularAnimation.prototype.constructor = CircularAnimation;
+
+CircularAnimation.prototype.getTransformationMatrix = function(t){
+
+  var m = mt4.create();
+
+  mt4.tranlate(m,m,this.center);
+  //mt4.rotate(m,m) E necessario passar os angulos em graus
+  mt4.translate(m,m,[0,this.radius,0]);
+
+  return m;
+}
--- Project 2/reader/Animation/LinearAnimation.js
+++ Project 2/reader/Animation/LinearAnimation.js
@@ -4,6 +4,7 @@ function LinearAnimation(id, controlPoints, time){
   this.time = time;
   this.totalDistance = 0;
   this.velocity = 0;
+  this.currentPointMin = 0;
   setVariables();
 }
 
@@ -32,9 +33,24 @@ LinearAnimation.prototype.calculateVelocity = function () {
 
 };
 
-
 LinearAnimation.prototype.calculateRotation = function(point1, point2){
   // rotation on y
   return Math.atan2(point2[0]- point1[0], point2[2] - point1[2]);
 
+};
+
+LinearAnimation.prototype.getTransformationMatrix = function(currentTime){
+  var m = mat4.create();
+
+  var dist = currentTime * this.velocity;
+  var vec = [];
+  vec[0] = this.controlPoints[this.currentPointMin + 1][0] - this.controlPoints[this.currentPointMin][0];
+  vec[1] = this.controlPoints[this.currentPointMin + 1][1] - this.controlPoints[this.currentPointMin][1];
+  vec[2] = this.controlPoints[this.currentPointMin + 1][2] - this.controlPoints[this.currentPointMin][2];
+
+
+  mat4.tranlate(m,m,vec);
+  mat4.rotate(m,m,calculateRotation(this.controlPoints[this.currentPointMin], this.controlPoints[this.currentPointMin+1]));
+  this.currentPointMin++;
+  return m;
 };
