a81a1f75ebd68e943504e3c656ec0e415d140b12
Caramelo18 <fabiocaramelo18@gmail.com>
2016-12-12 13:03:26 +0000

    tiles completed

--- Project 3/reader/Primitive/Tiles/Tile10.js
+++ Project 3/reader/Primitive/Tiles/Tile10.js
@@ -4,8 +4,21 @@
 */
 
 function Tile10(scene) {
- CGFobject.call(this,scene);
+    CGFobject.call(this,scene);
 
+    this.base = new Rectangle(scene, 0, 0.70, 0, 0.70);
+    this.cylinder = new Cylinder(scene, 0.2, 0.2, 0.1, 20, 20);
+
+
+    this.line = 0;
+    this.col = 0;
+
+
+    this.baseApp = new CGFappearance(scene);
+    this.baseApp.setDiffuse(0.3,0.3,0.3,1);
+
+    this.cylApp = new CGFappearance(scene);
+    this.cylApp.loadTexture('../textures/camo.jpg');
 
 };
 
@@ -14,5 +27,18 @@ Tile10.prototype.constructor = Tile10;
 
 Tile10.prototype.display = function () {
 
+    this.scene.translate(-2.5,1.666, 0); // place tile on 0,0
+    this.scene.pushMatrix();
+
+        this.scene.translate(0,0,0.01); //translation to avoid overlapping
+        this.scene.translate(0.065, 0.065, 0); // center tile
+        this.baseApp.apply();
+        this.base.display();
+
+        this.scene.translate(0.35,0.35,0);
+        this.cylApp.apply();
+        this.cylinder.display();
+
+    this.scene.popMatrix();
 
 };
